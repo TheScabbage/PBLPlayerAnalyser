a9f31a0a59da7b2d0d791d1d1176d46064dccb57
Scabbage <TheScabbage@gmail.com>
2018-11-07 21:45:29 +0800

    Added a converter program for preprocessing the CSV files generated from the player data spreadsheet.

---
+++ CSVConversion/CSVConverter/CSVConverter/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace CSVConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 1)
+            {
+                return;
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("No such file exists.");
+            }
+            else
+            {
+                string result = "";
+                try
+                {
+
+                    using (StreamReader fs = File.OpenText(args[0]))
+                    {
+                        string header = fs.ReadLine();
+                        string line = fs.ReadLine();
+
+                        while (line != null)
+                        {
+                            result += line;
+
+                            line = fs.ReadLine();
+
+                            if (line != null)
+                            {
+                                result += "\\n";
+                            }
+                        }
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                try
+                {
+                    if(!File.Exists(args[1]))
+                    {
+                        File.Create(args[1]);
+                    }
+                    using (StreamWriter fs = new StreamWriter(args[1]))
+                    {
+                        fs.Write(result);
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
