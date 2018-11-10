48484a38c93d9a710fd1328e39ba29d1c5ecd103
Scabbage <TheScabbage@gmail.com>
2018-11-10 21:33:59 +0800

    Refactored the search filter algorithm.

--- convert.bat
+++ convert.bat
@@ -1,4 +1,6 @@
 ECHO OFF
 .\CSVConversion\CSVConverter\CSVConverter\bin\Debug\CSVConverter.exe regseason.csv jsregseason.txt
 .\CSVConversion\CSVConverter\CSVConverter\bin\Debug\CSVConverter.exe playoffs.csv jsplayoffs.txt
+.\CSVConversion\CSVConverter\CSVConverter\bin\Debug\CSVConverter.exe playercosts.csv jsplayercosts.txt
+
 PAUSE
